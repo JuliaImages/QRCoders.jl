b9ee7636ed091afb9626f96e4244671b0eb817f6
Jérémie Gillet <jie.gillet@gmail.com>
2019-07-22 00:31:28 +0900

    Added exportqrcode

--- src/QRCode.jl
+++ src/QRCode.jl
@@ -7,6 +7,8 @@ export Mode, Numeric, Alphanumeric, Byte
 export ErrCorrLevel, Low, Medium, Quartile, High
 export getmode, getversion, qrcode, exportqrcode
 
+using Images
+
 """
 Abstract type that groups the three supported encoding modes `Numeric`,
 `Alphanumeric` and `Byte`.
@@ -82,8 +84,10 @@ end
 
 Return the version of the QR code, between 1 and 40.
 
+```jldoctest
 julia> getversion("Hello World!", Alphanumeric(), High())
 2
+```
 """
 function getversion(message::AbstractString, mode::Mode, level::ErrCorrLevel)::Int64
     return findfirst(v->v >= length(message), characterscapacity[(level, mode)])
@@ -337,9 +341,38 @@ function qrcode( message::AbstractString
 end
 
 
+"""
+    exportqrcode( message::AbstractString
+                , path = "qrcode.png"
+                , eclevel = Medium()
+                ; targetsize = 5
+                , compact = false )
 
-function exportqrcode()
+Create a `PNG` file with the encoded `message` of approximate size `targetsize`
+cm. If `compact` is `false`, white space is added around the QR code.
 
+The error correction level `eclevel` can be picked from four values: `Low()`
+(7% of missing data can be restored), `Medium()` (15%), `Quartile()` (25%) or
+`High()` (30%). Higher levels make denser QR codes.
+"""
+function exportqrcode( message::AbstractString
+                     , path::AbstractString = "qrcode.png"
+                     , eclevel::ErrCorrLevel = Medium()
+                     ; targetsize = 5
+                     , compact::Bool = false )
+
+    matrix = qrcode(message, eclevel, compact = compact)
+
+    if !endswith(path, ".png")
+        path = "$path.png"
+    end
+
+    # Seems the default setting is 72 DPI
+    pixels = size(matrix, 1)
+    scale = ceil(Int64, 72 * targetsize / 2.45 / pixels)
+    matrix = kron(matrix, trues((scale, scale)))
+
+    save(path, colorview(Gray, .! matrix))
 end
 
 end # module
