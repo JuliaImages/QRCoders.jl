703430dd71a92bbd2fae81276d48151689e7a00d
Lars Hellemo <Lars.Hellemo@sintef.no>
2021-04-01 08:02:27 +0200

    Split out image generation from show Rotate svg same as png

--- src/types.jl
+++ src/types.jl
@@ -11,28 +11,21 @@ function Base.show(io::IO, ::MIME"text/plain", s::QRCode)
                         width=size(qrm,2),colormap=[(1,1,1),(0,0,0)],labels=false))
 end
 
-function Base.show(io::IO, ::MIME"image/png", s::QRCode)
-    qrm = qrcode(s.s, s.eclevel)
+function Base.show(io::IO, ::MIME"image/png", qrc::QRCode)
+    write(io, qrpng(qrc))
+end
+
+function Base.show(io::IO, ::MIME"image/svg+xml", qrc::QRCode)
+    write(io, qrsvg(qrc))
+end
+
+function qrpng(qrc::QRCode)
+    qrm = qrcode(qrc.s, qrc.eclevel)
     scale = Int(round(200/size(qrm,1)))
     qrm = Gray.(repeat(.!qrm, inner=(scale,scale)))
     png = IOBuffer()
     save(Stream(format"PNG", png), qrm)
-    write(io, take!(png))
-end
-
-function Base.show(io::IO, ::MIME"image/svg+xml", s::QRCode)
-    qrm = qrcode(s.s, s.eclevel)
-    write(io, qrsvg(s))
-end
-
-function qr(txt::String)
-    qrm = plainqr(txt)
-    scale = Int(round(200/size(qrm,1)))
-    return Gray.(repeat(.!(plainqr(txt)),inner=(scale,scale)))
-end
-
-function plainqr(txt::String)
-    return qrcode(txt)
+    return take!(png)
 end
 
 function qrsvg(qrc::QRCode)
@@ -40,7 +33,7 @@ function qrsvg(qrc::QRCode)
     qrm = qrcode(qrc.s, qrc.eclevel)
     I, J = size(qrm)
     composition = compose(context(), fill("black"),
-    ((context((i-1)/I,(j-1)/J,1/I + 1e-3,1/J + 1e-3), rectangle()) 
+    ((context((j-1)/I,(i-1)/J,1/J + 1e-3,1/I + 1e-3), rectangle()) 
         for i=1:size(qrm,1),j=1:size(qrm,2) if qrm[i,j]==1 )...
     )
     composition |> SVG(svg, 5cm, 5cm)
